b76f0f365940eb58438e2caf78b0520e038ca1cb
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-18 23:10:08 -0400

    fixed an issue with the footer on the contact page after form submission

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,12 @@ formSubmitBtn.on("click", function(event){
     event.preventDefault();
     var name = $("#name").val();
     console.log(name);
-    formDiv.empty();
+    $("form").addClass("hidden");
 
     var row1Div = $("<div>").addClass("row");
     var row1Col = $("<div>").addClass("col-sm-12");
     var thankYouH1 = $("<h1>").text("Thank you " + name + "!");
+
     row1Col.append(thankYouH1);
     row1Div.append(row1Col);
     var row2Div = $("<div>").addClass("row");
@@ -18,5 +19,5 @@ formSubmitBtn.on("click", function(event){
     formDiv.addClass("text-center");
     row2Col.append(contactH2);
     row2Div.append(row2Col);
-    formDiv.append(row1Div, row2Div);
+    formDiv.prepend(row1Div, row2Div);
 })
